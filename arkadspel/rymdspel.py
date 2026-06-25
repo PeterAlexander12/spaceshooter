ab722016ee5753fab1019a2d3e2f4444f8d63c2a
PA_li <olovlindgren68@gmail.com>
2026-06-25 14:25:17 +0200

    fgdgdzgd

--- arkadspel/rymdspel.py
+++ arkadspel/rymdspel.py
@@ -7,7 +7,7 @@ WIDTH = 600
 HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-
+# ghrejheykytjzxb
 # load pictures
 player_pic = pygame.image.load("images/spelare.png").convert_alpha()
 enemy_pic = pygame.image.load("images/fiende.png").convert_alpha()
