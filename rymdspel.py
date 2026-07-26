db220bf521a9be9de5063fac8def270338a95ad3
Peter Lindholm <peteralexander112@protonmail.com>
2026-07-26 10:05:36 +0200

    remove unused images, rename image files for consistency

--- rymdspel.py
+++ rymdspel.py
@@ -12,7 +12,7 @@ clock = pygame.time.Clock()
 # load pictures
 player_pic = pygame.transform.scale(pygame.image.load("images/spelare.png").convert_alpha(), (50, 50))
 enemy_pic = pygame.transform.scale(pygame.image.load("images/fiende.png").convert_alpha(), (50, 50))
-potion_pic = pygame.transform.scale(pygame.image.load("images/Red_potion.png").convert_alpha(), (30, 30))
+potion_pic = pygame.transform.scale(pygame.image.load("images/red_potion.png").convert_alpha(), (30, 30))
 explosion_pic = pygame.image.load("images/nuclear_explosion.png").convert_alpha()
 bomb_pic = pygame.transform.scale(explosion_pic, (30, 30))
 missile_pic = pygame.image.load("images/bullet.png").convert_alpha()
